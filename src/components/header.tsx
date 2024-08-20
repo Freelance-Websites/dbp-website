17547b14d84b588f425965ea55a4fcb578d0b678
Juan Martin Garcia <hi@juanmartingarcia.com>
2024-08-20 12:41:32 -0300

    Responsive header styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import Image from 'next/image';
-import React from 'react';
 
 export default function Header({
   activeLanguage,
@@ -8,12 +8,14 @@ export default function Header({
   activeLanguage: String;
   changeLanguage: (lang: string) => void;
 }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="bg-gray-800 p-4">
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container mx-auto flex md:items-center justify-between">
         <a
           href="#top"
-          className="transition duration-300 ease-in-out hover:opacity-70"
+          className="transition duration-300 ease-in-out hover:opacity-70 relative top-1 md:top-0"
         >
           <Image
             src="/images/logo-white.svg"
@@ -22,40 +24,40 @@ export default function Header({
             height={18}
           />
         </a>
-        <nav className="flex gap-4 items-center">
-          <ul className="flex gap-4">
+        <nav className="flex gap-4 items-end md:items-center flex-col md:flex-row w-screen md:w-auto">
+          <ul className={`gap-4 ${isOpen === true ? 'flex flex-col my-8 w-full text-center order-last relative -left-8' : 'hidden md:flex'}`}>
             <li
-              className="uppercase text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
+              className="uppercase md:text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
             >
               <a href="#about">
                 {activeLanguage === 'es' ? 'Nosotros' : 'About Us'}
               </a>
             </li>
             <li
-              className="uppercase text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
+              className="uppercase md:text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
             >
               <a href="#offices">
                 {activeLanguage === 'es' ? 'Oficinas' : 'Offices'}
               </a>
             </li>
             <li
-              className="uppercase text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
+              className="uppercase md:text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
             >
               <a href="#services">
                 {activeLanguage === 'es' ? 'Servicios' : 'Services'}
               </a>
             </li>
             <li
-              className="uppercase text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
+              className="uppercase md:text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
             >
               <a href="/contact">
                 {activeLanguage === 'es' ? 'Contacto' : 'Contact'}
               </a>
             </li>
           </ul>
-          <ol className="flex gap-2 items-center">
-            <li className="transition duration-300 ease-in-out hover:opacity-70 h-6">
-            <button onClick={() => changeLanguage('es')}>
+          <ol className="flex gap-4 md:gap-2 items-center">
+            <li className="transition duration-300 ease-in-out hover:opacity-70">
+            <button className="flex items-center" onClick={() => changeLanguage('es')}>
                 <Image
                   src="/images/flag-es.svg"
                   alt={activeLanguage === 'es' ? 'Bandera de España. Al hacer clic cambiará el idioma a español.' : 'Spanish Flag. Clicking on it will change the language to Spanish.'}
@@ -64,14 +66,19 @@ export default function Header({
                 />
               </button>
             </li>
-            <li className="transition duration-300 ease-in-out hover:opacity-70 h-6">
-              <button onClick={() => changeLanguage('en')}>
+            <li className="transition duration-300 ease-in-out hover:opacity-70">
+              <button className="flex items-center" onClick={() => changeLanguage('en')}>
                 <Image
                   src="/images/flag-us.svg"
                   alt={activeLanguage === 'es' ? 'Bandera de USA. Al hacer clic cambiará el idioma a inglés.' : 'USA Flag. Clicking on it will change the language to English.'}
                   width={24}
                   height={24}
                 />
+              </button>
+            </li>
+            <li className="transition duration-300 ease-in-out hover:opacity-70 md:hidden">
+              <button className="flex items-center" onClick={() => setIsOpen(!isOpen)}>
+                <svg fill="none" height="18" viewBox="0 0 24 18" width="24" xmlns="http://www.w3.org/2000/svg" className="fill-white"><g><path d="m0 8h24v2h-24z"/><path d="m0 0h24v2h-24z"/><path d="m0 16h24v2h-24z"/></g></svg>
               </button>
             </li>
           </ol>
--- src/content/index.md
+++ src/content/index.md
@@ -1,6 +1,12 @@
 ---
-sections:
-  - type: hero
-    video: /images/video.mp4
-    title: Delta Bridge Partners
+en:
+  sections:
+    - type: hero
+      video: /images/video.mp4
+      title: Delta Bridge Partners
+es:
+  sections:
+    - type: hero
+      video: /images/video.mp4
+      title: Delta Bridge Partners
 ---
