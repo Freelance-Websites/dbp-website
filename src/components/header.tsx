7a7a99c707884e2cbbed892c80cfa30660354b8a
Juan Martin Garcia <hi@juanmartingarcia.com>
2024-08-20 12:01:16 -0300

    Basic language translation functionality

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,82 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 
-const Header: React.FC = () => {
+export default function Header({
+  activeLanguage,
+  changeLanguage
+}: {
+  activeLanguage: String;
+  changeLanguage: (lang: string) => void;
+}) {
   return (
-    <header className="flex items-center justify-between bg-gray-800">
-      <Link
-        href="#top"
-      >
-        <Image
-          src="/images/logo-white.svg"
-          alt="Delta Bridge Partners Logo"
-          width={80}
-          height={18}
-        />
-      </Link>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
+    <header className="bg-gray-800 p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <a
+          href="#top"
+          className="transition duration-300 ease-in-out hover:opacity-70"
+        >
+          <Image
+            src="/images/logo-white.svg"
+            alt="Delta Bridge Partners Logo"
+            width={80}
+            height={18}
+          />
+        </a>
+        <nav className="flex gap-4 items-center">
+          <ul className="flex gap-4">
+            <li
+              className="uppercase text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
+            >
+              <a href="#about">
+                {activeLanguage === 'es' ? 'Nosotros' : 'About Us'}
+              </a>
+            </li>
+            <li
+              className="uppercase text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
+            >
+              <a href="#offices">
+                {activeLanguage === 'es' ? 'Oficinas' : 'Offices'}
+              </a>
+            </li>
+            <li
+              className="uppercase text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
+            >
+              <a href="#services">
+                {activeLanguage === 'es' ? 'Servicios' : 'Services'}
+              </a>
+            </li>
+            <li
+              className="uppercase text-xs tracking-widest font-medium text-white hover:opacity-70 transition duration-300 ease-in-out"
+            >
+              <a href="/contact">
+                {activeLanguage === 'es' ? 'Contacto' : 'Contact'}
+              </a>
+            </li>
+          </ul>
+          <ol className="flex gap-2 items-center">
+            <li className="transition duration-300 ease-in-out hover:opacity-70 h-6">
+            <button onClick={() => changeLanguage('es')}>
+                <Image
+                  src="/images/flag-es.svg"
+                  alt={activeLanguage === 'es' ? 'Bandera de España. Al hacer clic cambiará el idioma a español.' : 'Spanish Flag. Clicking on it will change the language to Spanish.'}
+                  width={24}
+                  height={24}
+                />
+              </button>
+            </li>
+            <li className="transition duration-300 ease-in-out hover:opacity-70 h-6">
+              <button onClick={() => changeLanguage('en')}>
+                <Image
+                  src="/images/flag-us.svg"
+                  alt={activeLanguage === 'es' ? 'Bandera de USA. Al hacer clic cambiará el idioma a inglés.' : 'USA Flag. Clicking on it will change the language to English.'}
+                  width={24}
+                  height={24}
+                />
+              </button>
+            </li>
+          </ol>
+        </nav>
+      </div>
     </header>
   );
 };
-
-export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Inter } from "next/font/google";
 
 import Header from '@/components/header';
@@ -7,11 +8,27 @@ import { attributes } from '@/content/index.md';
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { en, es } = attributes;
+  const [language, setLanguage] = useState('en');
+
+  const changeLanguage = (lang: string) => {
+    setLanguage(lang);
+    window.localStorage.setItem('language', lang);
+  }
+
+  useEffect(() => {
+    const storedLanguage = window.localStorage.getItem('language');
+    changeLanguage(storedLanguage ? storedLanguage : 'en');
+  }, []);
+
   return (
     <main
       className={`${inter.className}`}
     >
-      <Header />
+      <Header
+        activeLanguage={language}
+        changeLanguage={changeLanguage}
+      />
       <p>test</p>
     </main>
   );
